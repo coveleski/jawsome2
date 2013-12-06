548f3985ec84d6aa5f588902881f4f33350aa7a0
David Vriezen <browzilla@gmail.com>
2013-12-05 18:43:20 -0600

    no message

--- simVM/src/com/cs352/Frame.java
+++ simVM/src/com/cs352/Frame.java
@@ -2,12 +2,11 @@ package com.cs352;
 
 import java.util.Stack;
 
-/**
- * Created by David on 12/4/13.
- */
 public class Frame {
+
+    byte[] frame_memory;
     Frame nextf;
     Frame prevf;
 
-    
+
 }
